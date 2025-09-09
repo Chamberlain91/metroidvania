f7ca8cbf7455f69ba8329e18b869dcf671b0ad58
Christopher Chamberlain <chamberlain@live.ca>
2025-09-08 23:27:40 -0600

    Updated dependency.

--- src/shaders/world.frag.hlsl
+++ src/shaders/world.frag.hlsl
@@ -5,14 +5,12 @@ Sprite_Push_Constants push;
 
 struct VertexOutput {
   float4 position : SV_POSITION;
-  float2 uv       : UV;
-  int    image    : IMAGE_INDEX;
+  float2 uv : UV;
+  int image : IMAGE_INDEX;
 };
 
 float4 main(VertexOutput vertex) : SV_Target {
-
   Texture2D tex = getTexture2D(vertex.image);
   SamplerState ss = getSamplerState(0);
-
   return tex.Sample(ss, vertex.uv);
 }
